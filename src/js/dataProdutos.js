966784796437d23aff7e204e9222ae2437518db6
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2022-07-29 23:49:42 -0300

    entrega concluida

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,37 +1,31 @@
-const produtos = [
-    {
-        nome: 'Banana',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: "./src/img/banana.png"
-    },
-    {
-        nome: 'Morango',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/morango.png'
-    },
-    {
-        nome: 'Maçã',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/maça.png'
-    },
-    {
-        nome: 'Pão',
-        preco: 4,
-        secao: 'Panificadora',
-        categoria: 'Pães',
-        img: './src/img/pao.png'
-    },
-    {
-        nome: 'Leite',
-        preco: 5,
-        secao: 'Laticínio',
-        categoria: 'Leite',
-        img: './src/img/leite.png'
+const listaProducts = [];
+
+
+function createItem(nome, preco, secao, categoria, img) {
+    let item = {
+        nome: nome,
+        preco: preco,
+        secao: secao,
+        categoria: categoria,
+        img: img
     }
-]
+
+    return item
+}
+
+
+function createProductsItem(objeto) {
+    listaProducts.push(objeto)
+};
+
+
+let banana = createItem("Banana", 2.00, "Hortifruti", "fruta", "./src/img/banana.png");
+let morango = createItem("Morango", 2.00, "Hortifruti", "fruta", "./src/img/morango.png");
+let maca = createItem("Maçã", 2.00, "Hortifruti", "fruta", "./src/img/maça.png");
+let pao = createItem("Pão", 4.00, "Panificadora", "Pães", "./src/img/pao.png");
+let leite = createItem("Leite", 5.00, "Laticínio", "Leite", "./src/img/leite.png");
+
+createProductsItem(banana);
+createProductsItem(morango);
+createProductsItem(pao);
+createProductsItem(leite);
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,128 @@
-// seu código aqui
+const ul = document.querySelector("ul")
+const inputBusca = document.querySelector(".campoBuscaPorNome")
+const btnBusca = document.querySelector("#pesquisa")
+const btnTodosProdutos = document.querySelector("#todosProdutos")
+const btnHortifruti = document.querySelector("#hortiFruti")
+const btnPanificadora = document.querySelector("#panificadora")
+const btnLaticinios = document.querySelector("#laticinios")
+const total = document.querySelector("#total")
+const quantidadeDeProduto = 0
+const valorTotal = 0
+
+function criaLista(itens) {
+
+    itens.forEach(element => {
+        let li = document.createElement("li")
+        let img = document.createElement("img")
+        let h3 = document.createElement("h3")
+        let span = document.createElement("span")
+        let p = document.createElement("p")
+
+        img.src = element.img
+        h3.innerText = element.nome
+        span.innerText = element.secao
+        p.innerText = `R$ ${element.preco},00`
+
+        ul.appendChild(li)
+        li.appendChild(img)
+        li.appendChild(h3)
+        li.appendChild(span)
+        li.appendChild(p)
+
+    });
+
+}
+
+criaLista(listaProducts)
+
+
+function todosProdutos() {
+
+    const mostrarTodos = listaProducts.filter((produtos) => {
+        return produtos.secao
+
+    })
+    ul.innerHTML = ""
+    criaLista(mostrarTodos)
+}
+
+
+btnTodosProdutos.addEventListener("click", todosProdutos);
+
+function hortifruti() {
+
+    const mostrarHortifruti = listaProducts.filter((produtos) => {
+        return produtos.secao === "Hortifruti"
+
+    })
+    ul.innerHTML = ""
+    criaLista(mostrarHortifruti)
+}
+
+
+btnHortifruti.addEventListener("click", hortifruti);
+
+
+function panificadora() {
+
+    const mostrarPanificadora = listaProducts.filter((produtos) => {
+        return produtos.secao === "Panificadora"
+
+
+    })
+    ul.innerHTML = ""
+    criaLista(mostrarPanificadora)
+}
+
+btnPanificadora.addEventListener("click", panificadora);
+
+
+function laticinios() {
+
+    const mostrarLaticinios = listaProducts.filter((produtos) => {
+        return produtos.secao === "Laticínio"
+
+    })
+    ul.innerHTML = ""
+    criaLista(mostrarLaticinios)
+}
+
+
+btnLaticinios.addEventListener("click", laticinios);
+
+
+
+
+function pesquisa() {
+
+    let resultadoBusca = []
+    ul.innerHTML = ""
+    for (let i = 0; i < listaProducts.length; i++) {
+        if (inputBusca.value == listaProducts[i].nome) {
+            resultadoBusca.push(listaProducts[i])
+        }
+    }
+
+
+    criaLista(resultadoBusca)
+
+}
+
+
+btnBusca.addEventListener("click", pesquisa)
+
+
+
+function precoTotal() {
+    const quantidadeDeProduto = listaProducts.reduce((valorAnterior, valorAtual) => {
+        const soma = {
+            valorTotal: 0
+        }
+        soma.preco = valorAnterior.preco + valorAtual.preco
+        return soma
+
+    })
+
+}
+
+precoTotal()
